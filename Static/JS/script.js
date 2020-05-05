db3385270a7846a2c69bd73b2aac473ea7a5681b
Preston Keomany <61899395+prestonkeo@users.noreply.github.com>
2020-05-04 20:03:15 -0600

    Update script.js

--- Static/JS/script.js
+++ Static/JS/script.js
@@ -1,12 +1,14 @@
 var symbol = "";
 
-var apiKey = "K0529GI9C27OCRIL";
+var apiKey = "OQSE2W0XD2C5HX0Z";
 
 var keyWord = "";
 
-var secondURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + symbol + "&apikey=" + apiKey;
+var searchWord = "";
 
-var thirdURL = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + keyWord + "&apikey=" + apiKey;
+var contCode = "";
+
+var contName = "";
 
 function getCurrent(sym) {
     symbol = sym;
@@ -27,12 +29,6 @@ function getCurrent(sym) {
         else if (sym === "AAPL") {
             $("#price4").text(response['Global Quote']['05. price'])
         }
-        else if (sym === "TSLA") {
-            $("#price5").text(response['Global Quote']['05. price'])
-        }
-        else if (sym === "BABA") {
-            $("#price6").text(response['Global Quote']['05. price'])
-        }
     });
 }
 
@@ -40,5 +36,62 @@ getCurrent("GOOG");
 getCurrent("MSFT");
 getCurrent("AMZN");
 getCurrent("AAPL");
-getCurrent("TSLA");
-getCurrent("BABA");
+
+$("#first-search").on("click", function () {
+    searchWord = $("#stockSearch").val()
+    showResults();
+});
+
+$('#stockSearch').keydown(function (e) {
+    if (e.keyCode == 13) {
+        e.preventDefault();
+        searchWord = $("#stockSearch").val()
+        showResults();
+    }
+});;
+
+function showResults() {
+    $("#searchResults").empty();
+    $("#searchThing").text("Searched Results For: " + searchWord);
+    keyWord = searchWord;
+    var thirdURL = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + keyWord + "&apikey=" + apiKey
+    $.ajax({
+        url: thirdURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var results = response.bestMatches;
+        for (var i = 0; i < results.length; i++) {
+            var resultDiv = $("<div>");
+            resultDiv.attr("class", "card col s6 teal lighten-5");
+
+            var name = results[i]['2. name'];
+            var symb = results[i]['1. symbol'];
+
+            var head3 = $("<h3>").text(name).attr("class", "flow-text");
+            var head4 = $("<h4>").text(symb).attr("class", "flow-text");
+
+            var proceed = $("<a>")
+            proceed.attr("class", "waves-effect waves-light btn-large").css("margin", "15px");
+            proceed.attr("value", results[i]['1. symbol']);
+            proceed.attr("title", results[i]['2. name']);
+            proceed.text("CONTINUE");
+
+            resultDiv.append(head3);
+            resultDiv.append(head4);
+            resultDiv.append(proceed);
+
+            $("#searchResults").append(resultDiv);
+
+        }
+        $(".btn-large").on("click", function () {
+            contCode = $(this).attr("value");
+            contName = $(this).attr("title");
+            localStorage.setItem("searched", contCode);
+            localStorage.setItem("named", contName);
+            $(".btn-large").attr("href", "main.html");
+            newCardInfo();
+        })
+
+    });
+}
