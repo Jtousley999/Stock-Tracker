e9d0f5a6d899220aeb2c05696f4026294d7292dc
Preston Keomany <61899395+prestonkeo@users.noreply.github.com>
2020-05-04 20:30:35 -0600

    Update main.js

--- Static/JS/main.js
+++ Static/JS/main.js
@@ -44,7 +44,7 @@ $(".btn-large").on("click", function(){
 });
 
 function loadSearched(){
-    var queryURL = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + saved +"&apikey=" + apiKey; 
+    queryURL = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + saved +"&apikey=" + apiKey; 
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -113,6 +113,141 @@ function showResults(){
             newCardInfo();        
         })
 
+    });
+}
+
+loadSearched();
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(updatePage);
+
+
+
+function buildQueryURL() {
+    // queryURL is the url we'll use to query the API
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+  
+    // Begin building an object to contain our API call's query parameters
+    // Set the API key
+    var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
+  
+    // Grab text the user typed into the search input, add to the queryParams object
+    queryParams.q = named;
+  
+    // If the user provides a startYear, include it in the queryParams object
+    var startYear = "2018";
+  
+    if (parseInt(startYear)) {
+      queryParams.begin_date = startYear + "0101";
+    }
+  
+    // If the user provides an endYear, include it in the queryParams object
+    var endYear = "2021";
+  
+    if (parseInt(endYear)) {
+      queryParams.end_date = endYear + "0101";
+    }
+  
+    // Logging the URL so we have access to it for troubleshooting
+    console.log("---------------\nURL: " + queryURL + "\n---------------");
+    console.log(queryURL + $.param(queryParams));
+    return queryURL + $.param(queryParams);
+  }
+  
+  /**
+   * takes API data (JSON/object) and turns it into elements on the page
+   * @param {object} NYTData - object containing NYT API data
+   */
+  function updatePage(NYTData) {
+    // Get from the form the number of results to display
+    // API doesn't have a "limit" parameter, so we have to do this ourselves
+    var numArticles = 5;
+  
+    // Log the NYTData to console, where it will show up as an object
+    console.log(NYTData);
+    console.log("------------------------------------");
+  
+    // Loop through and build elements for the defined number of articles
+    for (var i = 0; i < numArticles; i++) {
+      // Get specific article info for current index
+      var article = NYTData.response.docs[i];
+  
+      // Increase the articleCount (track article # - starting at 1)
+      var articleCount = i + 1;
+  
+      // Create the  list group to contain the articles and add the article content for each
+      var $articleList = $("<ul>");
+      $articleList.addClass("list-group");
+  
+      // Add the newly created element to the DOM
+      $("#article-section").append($articleList);
+  
+      // If the article has a headline, log and append to $articleList
+      var headline = article.headline;
+      var $articleListItem = $("<li class='list-group-item articleHeadline'>");
+  
+      if (headline && headline.main) {
+        console.log(headline.main);
+        $articleListItem.append(
+          "<span class='label label-primary'>" +
+            articleCount +
+            "</span>" +
+            "<strong> " +
+            headline.main +
+            "</strong>"
+        );
+      }
+  
+      // If the article has a byline, log and append to $articleList
+      var byline = article.byline;
+  
+      if (byline && byline.original) {
+        console.log(byline.original);
+        $articleListItem.append("<h5>" + byline.original + "</h5>");
+      }
+  
+      // Log section, and append to document if exists
+      var section = article.section_name;
+      console.log(article.section_name);
+      if (section) {
+        $articleListItem.append("<h5>Section: " + section + "</h5>");
+      }
+  
+      // Log published date, and append to document if exists
+      var pubDate = article.pub_date;
+      console.log(article.pub_date);
+      if (pubDate) {
+        $articleListItem.append("<h5>" + article.pub_date + "</h5>");
+      }
+  
+      // Append and log url
+      $articleListItem.append("<a href='" + article.web_url + "'>" + article.web_url + "</a>");
+      console.log(article.web_url);
+  
+      // Append the article
+      $articleList.append($articleListItem);
+    }
+  }
+
+  function goFor(){
+    clear();
+    var queryURL = buildQueryURL();
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(updatePage);
+  }
+
+  goFor();
+  
+  
+  // Function to empty out the articles
+  function clear() {
+    $("#article-section").empty();
+  }
+  
     });
 }
 
